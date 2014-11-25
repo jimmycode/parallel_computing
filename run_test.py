3ba0817d4603aefe8a604588e8a1c624b6a920b6
Jimmy Wu <yuxiang.cs@gmail.com>
2014-11-25 09:23:07 +0800

    test finished

--- run_test.py
+++ run_test.py
@@ -8,15 +8,15 @@ os.system("nvcc -o prime_sieve prime_sieve.cu")
 csvfile = file("result.csv", "wb")
 writer = csv.writer(csvfile )
 
-for n in [100, 1000, 10000, 100000, 1000000, 10000000]:
-    for p in range(1, 33):
+for n in [100, 10000, 1000000]:
+    for p in range(1, 33, 2):
         for c in range(10, 110, 10):
             time_sum = 0
-            for i in range(10):
+            for i in range(5):
                 command = "./prime_sieve " + str(n) + " " + str(p) + " " + str(c)
                 output = commands.getstatusoutput(command)
                 time_sum += int(output[1])
-            time = time_sum / 10;
+            time = time_sum / 5;
             writer.writerow((n, p, c, time))
 
 writer.close()
